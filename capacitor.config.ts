66e3a0d7aa1c6782f221d355a25591683c7c2ff0
Santhoshkumar.B <bksanthoshkumar7812@gmail.com>
2026-08-14 08:42:29 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Santo Race Meet',
+  webDir: 'www'
+};
+
+export default config;
